5ad4f053e07dbc505c945652a065df1ae7c8be37
nikoloza <nika.tomadze@gmail.com>
2023-01-18 20:35:20 +0400

    added smbls packages

---
+++ packages/smbls/index.js
@@ -0,0 +1,9 @@
+'use strict'
+
+export { create } from 'domql'
+export { transformProps } from 'css-in-props'
+export * from '@symbo.ls/init'
+export * from '@symbo.ls/config-default'
+export * from '@symbo.ls/uikit'
+export * from '@symbo.ls/scratch'
+export * from '@symbo.ls/sync-client'
