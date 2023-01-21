e39ed04a2794f518cebaa5a65d8fb5ae4b718889
nikoloza <nika.tomadze@gmail.com>
2023-01-21 20:42:22 +0400

    fix pkg

--- packages/cli/bin/convert.js
+++ packages/cli/bin/convert.js
@@ -18,26 +18,30 @@ program
     .option('--angular', 'Convert all DomQL components to Angular')
     .option('--vue2', 'Convert all DomQL components to Vue2')
     .option('--vue3', 'Convert all DomQL components to Vue3')
-    .action(async (src, dest, react, angular, vue2, vue3) => {
-      const destParent = path.dirname(path.resolve(dest))
+    .action(async (src, dest, options) => {
+      const srcParent = path.dirname()
+      const destParent = path.dirname()
       const tmpDirPath = path.join(destParent, TMP_DIR_NAME)
-      await fs.mkdir(tmpDirPath)
 
-      //
-      await esbuild.build({
-        entryPoints: [path.join(src, '**.js')],
-        bundle: true,
-        outfile: path.join(tmpDirPath, "out.js")
+      await fs.mkdir(tmpDirPath, async () => {
+        const origFiles = await fs.promises.readdir(src)
+        console.log(origFiles)
+        
+        await esbuild.build({
+          entryPoints: origFiles.map(file => path.join(srcParent, file)),
+          bundle: true,
+          outdir: tmpDirPath
+        })
+  
+      //   const files = await fs.promises.readdir(tmpDirPath)
+      //   for (const file of files) {
+      //     let filePath = path.join(tmpDirPath, file)
+  
+      //     if ((await fs.promises.stat(filePath)).isDirectory()) {
+      //       console.log(`importing ${filePath}`)
+      //       const domqlObj = await import(`${filePath}/index.js`)
+      //       console.log(domqlObj)
+      //     }
+      //   }
       })
-
-      const files = await fs.promises.readdir(tmpDirPath)
-      for (const file of files) {
-        let filePath = path.join(tmpDirPath, file)
-
-        if ((await fs.promises.stat(filePath)).isDirectory()) {
-          console.log(`importing ${filePath}`)
-          const domqlObj = await import(`${filePath}/index.js`)
-          console.log(domqlObj)
-        }
-      }
     })
