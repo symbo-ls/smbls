111105763e5ad4192cd5a9113ec61351d55e16c5
nikoloza <nika.tomadze@gmail.com>
2023-02-02 23:54:14 +0400

    export ordering for uikit

--- packages/smbls/index.js
+++ packages/smbls/index.js
@@ -3,9 +3,9 @@
 // export { transformProps } from 'css-in-props'
 export * from '@symbo.ls/default-config'
 export * from '@symbo.ls/scratch'
-export * from '@symbo.ls/uikit'
 export * from '@symbo.ls/init'
 export * from '@symbo.ls/emotion'
 export * from '@symbo.ls/create'
+export * from '@symbo.ls/uikit'
 export * from '@symbo.ls/socket/client'
 // export * from '@symbo.ls/socket-ui' // TODO: add it soon
