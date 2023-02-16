2adbf72536e6f171cc17df1ce5caf5978cb97d18
nikoloza <nika.tomadze@gmail.com>
2023-02-16 22:35:52 +0400

    removed collection caches

--- packages/create/define.js
+++ packages/create/define.js
@@ -22,10 +22,7 @@ export const defaultDefine = {
   },
 
   __filepath: param => param,
-  __collectionCache: param => param,
   $setCollection: Collection.define.$setCollection,
-  __stateCollectionCache: param => param,
   $setStateCollection: Collection.define.$setStateCollection,
-  __propsCollectionCache: param => param,
   $setPropsCollection: Collection.define.$setPropsCollection
 }
