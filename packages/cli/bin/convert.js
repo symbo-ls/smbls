48721ef25c306b1236b4ab63a1531a5d5d1d5567
Nika Otiashvili <nikaoto@gmail.com>
2023-02-23 11:24:46 +0400

    WIP cli convert command

--- packages/cli/bin/convert.js
+++ packages/cli/bin/convert.js
@@ -1,9 +1,8 @@
 'use strict'
 
 import * as esbuild from 'esbuild'
-import { loadModule } from './require.js'
 import { program } from './program.js'
-import { convert } from 'domql-to-mitosis/asd/convert.js'
+import { convert } from 'domql-to-mitosis'
 import fs from 'fs'
 import path from 'path'
 
@@ -19,40 +18,53 @@ program
     .option('--vue2', 'Convert all DomQL components to Vue2')
     .option('--vue3', 'Convert all DomQL components to Vue3')
     .action(async (src, dest, options) => {
-      const srcPath = path.resolve(src)
-      const destPath = path.resolve(dest || 'asd')
+      // Resolve source & destination directories
+      const srcPath = path.resolve(src || './src')
+      const destPath = path.resolve(dest || './dist')
       const tmpDirPath = path.resolve(path.dirname(destPath), TMP_DIR_NAME)
 
-      await fs.mkdir(tmpDirPath, async () => {
-        const origFiles = await fs.promises.readdir(srcPath)
-        console.log(origFiles)
-        
-        await esbuild.build({
-          entryPoints: origFiles.map(file => path.join(srcPath, file, '/index.js')),
-          bundle: true,
-          target: 'node12',
-          format: 'cjs',
-          outdir: tmpDirPath
-        })
-  
-        const files = await fs.promises.readdir(tmpDirPath)
-        for (const file of files) {
-          if (file === 'atoms') continue
+      // Make tmp directory
+      try {
+        await fs.promises.mkdir(tmpDirPath)
+      } catch (err) {
+        if (err.code !== 'EEXIST')
+          return 1;
+      }
 
-          let filePath = path.join(tmpDirPath, file)
-  
-          if ((await fs.promises.stat(filePath)).isDirectory()) {
-            console.log(`importing ${filePath}`)
-            const domqlModule = (await import(`${filePath}/index.js`)).default
-            console.log(domqlModule)
-            for (const key in domqlModule) {
-              const component = domqlModule[key]
-              console.group(key)
-              console.log('========================', key)
-              convert(component, 'react')
-              console.groupEnd(key)
-            }
+      const origFiles = await fs.promises.readdir(srcPath)
+      console.log(origFiles)
+
+      // Bundle components
+      await esbuild.build({
+        entryPoints: origFiles.filter(f => f !== "Atoms").map(file => path.join(srcPath, file, '/index.js')),
+        bundle: true,
+        target: 'node12',
+        format: 'cjs',
+        // Doing this prevents esbuild from including all the external deps we don't need
+        external: ['*'],
+        outdir: tmpDirPath
+      })
+      console.log("built")
+
+      // Convert components
+      const files = await fs.promises.readdir(tmpDirPath)
+      console.log(files)
+      for (const file of files) {
+        if (file === 'Atoms') continue
+
+        let filePath = path.join(tmpDirPath, file)
+
+        if ((await fs.promises.stat(filePath)).isDirectory()) {
+          console.log(`importing ${filePath}`)
+          const domqlModule = (await import(`${filePath}/index.js`)).default
+          console.log(domqlModule)
+          for (const key in domqlModule) {
+            const component = domqlModule[key]
+            console.group(key)
+            console.log('========================', key)
+            convert(component, 'react')
+            console.groupEnd(key)
           }
         }
-      })
+      }
     })
