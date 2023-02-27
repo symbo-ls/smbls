73847b36dea8e9a7c08917167e130d21cf5bf3dd
nikoloza <nika.tomadze@gmail.com>
2023-02-27 04:34:04 +0400

    reversed

--- packages/convert/index.js
+++ packages/convert/index.js
@@ -1,9 +1,9 @@
 'use strict'
 
-import { convert as conv } from 'domql-to-mitosis' // TODO: => @domql/mitosis
+// import { convert as conv } from 'domql-to-mitosis' // TODO: => @domql/mitosis
 import { create } from '@symbo.ls/create'
 
 export const convert = (component, dest, options) => {
   const element = create(component, options)
-  return conv(element, dest)
+  // return conv(element, dest)
 }
