1d8006ffe25b7ab7ec02bad940d8a5f84529acc9
nikoloza <nika.tomadze@gmail.com>
2023-03-04 01:21:06 +0400

    added .create def options

--- packages/convert/index.js
+++ packages/convert/index.js
@@ -3,7 +3,11 @@
 import { convert as conv } from 'domql-to-mitosis' // TODO: => @domql/mitosis
 import { create } from '@symbo.ls/create'
 
-export const convert = (component, dest, options) => {
+const CONVERT_OPTIONS = {
+  verbose: false
+}
+
+export const convert = (component, dest, options = CONVERT_OPTIONS) => {
   const element = create(component, options)
   return conv(element, dest)
 }
