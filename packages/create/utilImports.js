09cf1998f2695a1edf35ac8cb0cbea7d5de12af3
nikoloza <nika.tomadze@gmail.com>
2023-03-12 10:12:20 +0400

    adding extra utils

---
+++ packages/create/utilImports.js
@@ -0,0 +1,10 @@
+'use strict'
+
+export * from '@domql/utils'
+export * from '@symbo.ls/utils'
+export { set } from '@symbo.ls/scratch'
+export { init, updateReset } from '@symbo.ls/init'
+
+export * from '@domql/cookie'
+export * from '@domql/report'
+export * from '@domql/router'
