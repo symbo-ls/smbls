3ba2a9e2cf1a4b3690d14bbe6471c2ff7388002f
nikoloza <nika.tomadze@gmail.com>
2023-03-13 07:28:58 +0400

    typo to sync

--- packages/create/syncExtend.js
+++ packages/create/syncExtend.js
@@ -6,13 +6,16 @@ import { isProduction, isTest } from '@domql/env'
 const { NODE_ENV } = process.env
 
 export const applySyncDebug = (extend, options) => {
-  if (options.debug) extend.push[DevFocus]
-  if (options.socket) extend.push[Sync]
+  const { editor } = options
+  if (!editor) return extend
+  if (editor.debug) extend.push[DevFocus]
+  if (editor.socket) extend.push[Sync]
   return extend
 }
 
 export const applyKeyDebugListener = (root, options) => {
-  if (options.debug) inspectOnKey(root)
+  const { editor } = options
+  if (editor && editor.debug) inspectOnKey(root)
 }
 
 const extend = isProduction(NODE_ENV) || isTest(NODE_ENV)  [Sync, DevFocus]
