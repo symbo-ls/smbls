cf1d3811e944aaf07bd128847badcd84c3c1c3b4
nikoloza <nika.tomadze@gmail.com>
2023-03-13 07:34:48 +0400

    added sync params default to create

--- packages/create/syncExtend.js
+++ packages/create/syncExtend.js
@@ -1,15 +1,16 @@
 'use strict'
 
 import { Sync, DevFocus, inspectOnKey } from '@symbo.ls/socket-ui'
-import { isProduction, isTest } from '@domql/env'
-
-const { NODE_ENV } = process.env
+import { isDevelopment, isProduction, isTest } from '@domql/env'
+import { isUndefined } from '@domql/utils'
 
 export const applySyncDebug = (extend, options) => {
   const { editor } = options
   if (!editor) return extend
-  if (editor.inspect) extend.push[DevFocus]
-  if (editor.liveSync) extend.push[Sync]
+  const inspect = isUndefined(editor.inspect) ? isDevelopment() : editor.inspect
+  const liveSync = isUndefined(editor.liveSync) ? isDevelopment() : editor.liveSync
+  if (inspect) extend.push[DevFocus]
+  if (liveSync) extend.push[Sync]
   return extend
 }
 
