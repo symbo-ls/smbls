4060132fb7a2a5a0f26f436647dafe579a255f57
nikoloza <nika.tomadze@gmail.com>
2023-03-13 06:28:35 +0400

    fixed system import

--- packages/scratch/src/index.js
+++ packages/scratch/src/index.js
@@ -1,6 +1,7 @@
 'use strict'
 
 export * from './factory.js'
+export * from './defaultConfig'
+export * from './system'
 export * from './utils'
 export * from './set.js'
-export * from './defaultConfig'
