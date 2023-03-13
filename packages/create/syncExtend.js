0df2ab6be472dd48635f218219bd568fadad11f6
nikoloza <nika.tomadze@gmail.com>
2023-03-13 07:30:25 +0400

    changed sync params

--- packages/create/syncExtend.js
+++ packages/create/syncExtend.js
@@ -8,14 +8,14 @@ const { NODE_ENV } = process.env
 export const applySyncDebug = (extend, options) => {
   const { editor } = options
   if (!editor) return extend
-  if (editor.debug) extend.push[DevFocus]
-  if (editor.socket) extend.push[Sync]
+  if (editor.inspect) extend.push[DevFocus]
+  if (editor.liveSync) extend.push[Sync]
   return extend
 }
 
 export const applyKeyDebugListener = (root, options) => {
   const { editor } = options
-  if (editor && editor.debug) inspectOnKey(root)
+  if (editor && editor.inspect) inspectOnKey(root)
 }
 
 const extend = isProduction(NODE_ENV) || isTest(NODE_ENV)  [Sync, DevFocus]
