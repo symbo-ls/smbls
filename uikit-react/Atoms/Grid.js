cf30eae6e30dfcbd260579b94ccc3f983a92f9ab
nikoloza <nika.tomadze@gmail.com>
2023-03-16 03:37:29 +0400

    added grid props

--- uikit-react/Atoms/Grid.js
+++ uikit-react/Atoms/Grid.js
@@ -1,12 +1,13 @@
 'use strict'
 
 import React from "react"
+import { Grid } from "@symbo.ls/atoms";
 import { Box } from "@symbo.ls/react-box"
 import { transformEmotion, transformClassname} from 'css-in-props' 
 
 export const Grid = (props) => {
   const excludedProps = {};
-  const transformedProps = transformClassname(flexProps, void 0, Flex.class, excludedProps)
+  const transformedProps = transformClassname(flexProps, void 0, Grid.class, excludedProps)
   const propsClass = transformEmotion(transformedProps)
  
   return (
