383993d8e5e8383b1b26a5a921aea45a26d1ad4a
nikoloza <nika.tomadze@gmail.com>
2023-03-16 06:52:47 +0400

    fixed Box class passing

--- uikit-react/Box/index.js
+++ uikit-react/Box/index.js
@@ -1,29 +1,24 @@
 
-import React, { useEffect, useState } from "react";
+import React from "react";
 import { transformEmotion, transformClassname} from 'css-in-props'
 import { useGlobalTheme, useSymbols } from '@symbo.ls/react-provider'
-
-console.log(React)
 
 export const Box = (props) => {
   const context = useSymbols()
   const [theme, setTheme] = useGlobalTheme()
   const excludedProps = {};
 
-  console.log(props)
   const transformedProps = transformClassname(props, context, void 0, excludedProps)
-  console.log(transformedProps)
   const propsClass = transformEmotion(transformedProps)
-  console.log(propsClass)
-  const { tag, className } = props
-  const children = props.text ? props.text : props.children;
-  console.log(excludedProps)
+  
+  let { children, tag, className, text, ...restProps } = props
+  if (props.text) children = children.concat(text)
 
   return React.createElement(
     tag || "div",
     {
-      className: `${className ?? ""} ${propsClass}` ,
       ...excludedProps,
+      className: `${className ?? ""} ${propsClass}`,
     },
     children
   )
