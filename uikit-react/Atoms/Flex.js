137683ded33fd2e4e457a6e4092444d7182d1710
gugakatsi <guga.katsiashvili@gmail.com>
2023-03-18 17:54:37 +0400

    UikitReact: Flex media.js compatible

--- uikit-react/Atoms/Flex.js
+++ uikit-react/Atoms/Flex.js
@@ -4,10 +4,18 @@ import React from "react"
 import { Flex as smbmsFlex } from "@symbo.ls/atoms"
 import { Box } from "@symbo.ls/react-box"
 import { transformEmotion, transformClassname } from 'css-in-props'
+import { useSymbols } from '@symbo.ls/react-provider'
 
 export const Flex = (props) => {
+  const context = useSymbols();
+
+  const fakeElement = {
+    context,
+    __root: {}
+  };
   const excludedProps = {}
-  const transformedProps = transformClassname(props, void 0, smbmsFlex.class, excludedProps)
+
+  const transformedProps = transformClassname(props, void 0, smbmsFlex.class, excludedProps, fakeElement)
   const propsClass = transformEmotion(transformedProps)
 
   return (
