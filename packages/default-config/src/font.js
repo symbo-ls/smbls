12af0057e2eaacae106fb934314d29347f7c7cb3
nikoloza <nika.tomadze@gmail.com>
2023-04-09 15:59:35 +0400

    fixed isVariable def config

--- packages/default-config/src/font.js
+++ packages/default-config/src/font.js
@@ -7,14 +7,14 @@ const KarlaItalicVariable = '/Karla/Karla-VariableFont_wght.ttf' // eslint-disab
 export const FONT = {
   Karla: [{
     url: S3_BUCKET + KarlaVariable,
-    fontWeight: 'variable',
+    isVariable: true,
     fontStyle: 'normal',
     fontDisplay: 'swap',
     unicodeRange: 'U+0100-024F, U+0259, U+1E00-1EFF, U+2020, U+20A0-20AB, U+20AD-20CF, U+2113, U+2C60-2C7F, U+A720-A7FF'
   }, {
     url: S3_BUCKET + KarlaVariable,
+    isVariable: true,
     fontStyle: 'normal',
-    fontWeight: 'variable',
     fontDisplay: 'swap',
     unicodeRange: 'U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD'
   }]
