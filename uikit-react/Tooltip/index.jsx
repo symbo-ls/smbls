6f0430d95168f7f1a204659fb1adfba88cbfead0
nikoloza <nika.tomadze@gmail.com>
2023-04-09 08:19:55 +0400

    fixed subtitle

--- uikit-react/Tooltip/index.jsx
+++ uikit-react/Tooltip/index.jsx
@@ -1,69 +1,19 @@
-'use strict';
+'use strict'
 
-import React from 'react';
-import { Box } from '@symbo.ls/react-box';
-import { Flex } from '@symbo.ls/react-atoms';
+import React from 'react'
+import { Box } from '@symbo.ls/react-box'
+import { Flex } from '@symbo.ls/react-atoms'
+import { Tooltip as D } from "@symbo.ls/tooltip"
 
 export const Tooltip = (props) => {
-  const containerProps = {
-    flexDirection: 'column',
-
-    textAlign: 'center',
-    padding: 'Z1 A',
-    round: 'Y2',
-    minWidth: 'D2',
-    gap: 'X',
-
-    height: '36px',
-    background: 'tooltipBackground',
-    borderRadius: '12px',
-    color: 'pink',
-
-    shape: "tooltip",
-    shapeDirection: 'top',
-    shapeModifier: {
-      position: 'block east',
-      direction: 'north east',
-    },
-    ...props.containerProps,
-  };
-
-  const titleProps = {
-    fontWeight: '500',
-    color: 'gray12',
-    text: 'Tooltip is coming',
-    ...props.titleProps,
-  };
-
-  const subtitleProps = {
-    fontSize: 'Z2',
-    margin: '0',
-    color: 'gray6',
-    text: 'and winter too',
-    fontWeight: '400',
-    ...props.subtitleProps,
-  };
+  const { title, p, ...restProps } = props
 
   return(
-    <Flex
-      {...containerProps}
-    >
-      <Box
-        // tag={'title'}
-        {...titleProps}
-      >
-        {
-          titleProps.text
-        }
-      </Box>
-      <Box
-        tag={'p'}
-        {...subtitleProps}
-      >
-        {
-          subtitleProps.text
-        }
-      </Box>
+    <Flex {...restProps}>
+      <Box {...title}></Box>
+      <Box tag={'p'} { ...p }></Box>
     </Flex>
   )
-};
+}
+
+Tooltip.defaultProps = D.props
