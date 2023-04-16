072db837c211d4cb4fcf534e0ef7c3a1231160f8
Nika Otiashvili <nikaoto@gmail.com>
2023-04-16 20:05:26 +0400

    git rm -r react/ tmpreact/

--- react/component-index.js
+++
@@ -1,5 +0,0 @@
-import { Img } from './Atoms'
-
-export {
-  Img
-}
--- tmpreact/Sidebar/index.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import { css } from "@emotion/css";
-import { setClassname } from "css-in-props";
-
-export default function Sidebar(props) {
-  return <div />;
-}
-
