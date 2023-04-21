b2aadff4ec034a2b50d1ececd16f1c752c2863af
nikoloza <nika.tomadze@gmail.com>
2023-04-22 01:50:26 +0400

    create back with uikit

--- packages/smbls/index.js
+++ packages/smbls/index.js
@@ -4,8 +4,8 @@ export * from '@symbo.ls/default-config'
 export * from '@symbo.ls/scratch'
 export * from '@symbo.ls/emotion'
 export * from '@symbo.ls/init'
-export * from '@symbo.ls/uikit'
 export * from '@symbo.ls/create'
+export * from '@symbo.ls/uikit'
 // export { transformClassname } from 'css-in-props'
 // export * from '@symbo.ls/socket/client' // TODO: Check with @nikoloza
 // export * from '@symbo.ls/socket-ui' // TODO: add it soon
