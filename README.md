cabe8bb14b1f4220a34a16105450c35f0591ef74
nikoloza <nika.tomadze@gmail.com>
2023-04-24 00:07:30 +0400

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# DOMQL Starter Kit
+Example dev setup (boilerplate) to use [DOMQL](https://github.com/rackai/domql). You can also check [playground](https://domql.com/playground/).
+
+### Setup
+
+1. Clone the repo
+```
+git clone git@github.com:domql/starter-kit.git
+```
+
+2. Install scripts
+```
+yarn
+```
+
+3. Run the project
+```
+yarn start
+```
---
+++ src/index.js
@@ -0,0 +1,96 @@
+'use strict'
+
+import DOM from 'domql'
+import { Collection } from '@symbo.ls/atoms'
+var { performance } = window
+
+const state = {
+  title: 'domql',
+  opts: { val: 'opts' },
+  data: [
+    { name: 'render', icon: '✅' },
+    { name: 'render2', icon: '✅' },
+    { name: 'render3', icon: '✅' },
+  ]
+}
+
+DOM.create({
+  state,
+
+  h1: { text: (el, s) => s.title },
+  
+  el: {
+    extend: Collection,
+    state: 'data',
+    childExtend: { 
+      state: true,
+      div: {
+        attr: { contentEditable: true },
+        text: (el, s) => s.icon + ' ' + s.name,
+      },
+      div3: {
+        state: '../../title',
+        text: (el, s) => s.value,
+      },
+      div2: {
+        state: '../../opts/val',
+        text: (el, s) => s.value,
+      },
+      div4: {
+        text: (el, s) => {
+          return 'straight ' + s.parent.parent.title
+        }
+      },
+      button: {
+        text: 'remove',
+        on: {
+          click: (ev, el, s) => {
+            s.parent.remove(el.parent.key)
+          }
+        }
+      },
+      on: {
+        input: (ev, el, s) => {
+          s.update({ name: el.node.textContent })
+        }
+      }
+    },
+    $setPropsCollection: (el, state) => {
+      return state
+    }
+  },
+
+  button: {
+    text: 'add',
+    on: {
+      click: (ev, el, s) => {
+        s.data.push({ name: 'domql' + s.data.length, icon: '✅' })
+        s.update({ data: s.data })
+      }
+    }
+  },
+
+  input: {
+    on: {
+      input: (ev, el, s) => {
+        s.update({ title: el.node.value })
+      }
+    }
+  },
+
+  pre: {
+    text: (el, s) => JSON.stringify(s.parse(), null, 2)
+  },
+
+  mere: {
+    childExtend: {
+      text: (el, s) => s.name,
+    },
+    $setStateCollection: (el, state) => {
+      // console.warn(state.data)
+      // return state.data
+    }
+  }
+}, null, 'app')
+
+// dom.update({ time: `${performance.now() - start}` })
