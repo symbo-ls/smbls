06abbd8d7aa6a1e01d129919222297ff4d870e74
nikoloza <nika.tomadze@gmail.com>
2023-05-01 02:54:33 +0400

    cleanup smbls

--- packages/smbls/index.js
+++ packages/smbls/index.js
@@ -6,6 +6,6 @@ export * from '@symbo.ls/emotion'
 export * from '@symbo.ls/init'
 export * from '@symbo.ls/create'
 export * from '@symbo.ls/uikit'
-// export { transformClassname } from 'css-in-props'
+
 // export * from '@symbo.ls/socket/client' // TODO: Check with @nikoloza
 // export * from '@symbo.ls/socket-ui' // TODO: add it soon
