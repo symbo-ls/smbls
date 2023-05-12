ff572bce65ffa20e242e614facb8f5b8017416e7
Nika Otiashvili <nikaoto@gmail.com>
2023-05-12 14:10:53 +0400

    remove unused console.log

--- packages/cli/bin/convert.js
+++ packages/cli/bin/convert.js
@@ -228,7 +228,6 @@ program
     // Convert components
     const componentDirs = await fs.promises.readdir(tmpDirPath)
     for (const componentDir of componentDirs) {
-      console.log(componentDirs)
       const importDir = path.join(tmpDirPath, componentDir)
       if ((await fs.promises.stat(importDir)).isDirectory()) {
         // Import the module
