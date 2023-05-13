17740198491d4342b9d59b93c024865c259b95c1
Nika Otiashvili <nikaoto@gmail.com>
2023-05-13 18:20:33 +0400

    move kalduna script commands into build/kalduna.sh

--- build/kalduna.sh
+++ build/kalduna.sh
@@ -1,4 +1,9 @@
-rimraf -I uikit/react-generated/
-smbls convert uikit/domql/ uikit/react-genearted/ --react --internal-uikit
-cp -r build/kalduna/* uikit/react-generated/.
-echo 'Run `yarn && yarn start` in react/'
+#! /bin/bash
+
+GEN_DIR="uikit/react-generated"
+SRC_DIR="uikit/domql"
+
+rimraf -I "$GEN_DIR" && \
+    smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit && \
+    cp -r build/kalduna/* "${GEN_DIR}/." && \
+    echo "Run 'yarn && yarn start' in ${GEN_DIR}/"
