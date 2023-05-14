5c3518471e8907fdb8dffe11915f78cf67dd6615
Nika Otiashvili <nikaoto@gmail.com>
2023-05-14 21:26:13 +0400

    rm build/kalduna; update build/kalduna.sh

--- build/kalduna.sh
+++ build/kalduna.sh
@@ -1,9 +1,8 @@
 #! /bin/bash
+# This script MUST be run from the root of the smbls repository
 
-GEN_DIR="uikit/react-generated"
+GEN_DIR="${1:-uikit/react-generated}"
 SRC_DIR="uikit/domql"
 
 rimraf -I "$GEN_DIR" && \
-    smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit && \
-    cp -r build/kalduna/* "${GEN_DIR}/." && \
-    echo "Run 'yarn && yarn start' in ${GEN_DIR}/"
+    smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit
