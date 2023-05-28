73df18b771ff0c89e235d51b042f3644947b8532
gugakatsi <guga.katsiashvili@gmail.com>
2023-05-28 21:02:36 +0400

    update DomValueInterceptor

--- uikit/plugins/Editorjs/data/DomValueInterceptor.js
+++ uikit/plugins/Editorjs/data/DomValueInterceptor.js
@@ -29,7 +29,7 @@ const parseNode = (node) => {
 
   if (node.nodeType === Node.TEXT_NODE) {
     obj.type = 'text';
-    obj.value = node.nodeValue.trim();
+    obj.value = decodeHTMLSpaces(node.nodeValue.trim());
   } else if (node.nodeName === 'B') {
     obj.type = 'text-cosmetic';
     obj.textWeight = 'Bold';
@@ -40,7 +40,7 @@ const parseNode = (node) => {
     obj.value = 'italic value';
   } if (node.nodeName === 'A') {
     obj.type = 'link';
-    obj.link = node.getAttribute('href');
+    obj.url = node.getAttribute('href');
   } else if (node.nodeName === 'MARK') {
     obj.type = 'marker';
   }
@@ -62,11 +62,57 @@ const parseNodes = (childNodes) => {
   return currentLevel;
 }
 
+const decodeHTMLSpaces = (string) => {
+  return string.replace(/&nbsp;/g, '\u00A0');
+}
+
+const mutateItemsAccordingToRules = (data, mutatedItems) => {
+  const rules = {
+    'change-list-structure': {
+      conditional: (item) => (item.type === 'list'),
+      action: (item) => {
+        const itemInTopData = data[item.arrayIndex];
+        itemInTopData.type = 'list-nested-dom-values';
+
+        itemInTopData.data.items = itemInTopData.data.items.map((item, i) => {
+          if (typeof item === "string") {
+            return { type: 'text', value: item }
+          }
+
+          return item;
+        });
+
+      },
+    }, // Rename to 'DomValuesList', objectize
+    'change-mutated-paragraph-structure': {
+      conditional: (item) => (item.type === 'paragraph'),
+      action: (item) => {
+        data[item.arrayIndex].type = 'paragraph-nested-dom-values';
+      },
+    }, // Rename to DomValuesParagraph , objectize text
+  };
+
+  mutatedItems.forEach((item, pIndex) => {
+    Object.keys(rules).forEach((ruleKey) => {
+      const rule = rules[ruleKey];
+      if (rule['conditional'](item)) {
+        rule['action'](item);
+      }
+    })
+  });
+};
+
+const normalizeArray = () => {
+  // TODO
+  // Objectize array according to DomValues rules
+};
+
 const shouldParseHTML = (value) => (
   value.includes('<a') ||
   value.includes('<mark') ||
   value.includes('<b') ||
-  value.includes('<i')
+  value.includes('<i') ||
+  value.includes('&nbsp;')
 )
 
 const deepIterate = (obj, callback) => {
@@ -80,20 +126,35 @@ const deepIterate = (obj, callback) => {
   }
 }
 
+const deepMetaIterate = (data, callback) => {
+  for (let arrayIndex = 0; arrayIndex < data.length; arrayIndex++) {
+    const item = data[arrayIndex];
+    deepIterate(
+      item,
+      (obj, key, value) => {
+        callback({ arrayIndex, ...item }, obj, key, value)
+      }
+    );
+  }
+};
+
 export const DomValueInterceptor = (data) => {
   // deep iterate keys
   // check values if shouldParseHTML
   // parse if any html and convert to type = 'type-nested' if needed
-  deepIterate(data, (obj, key, value) => {
-    if (value) {
-      if (typeof value === 'string' && shouldParseHTML(value)) {
-        obj[key] = {
-          type: 'nested-block',
-          children: parseHtml(value),
-        }
-      }
+  const mutatedItems = [];
+
+  deepMetaIterate(data, (parent, obj, key, value) => {
+    if (typeof value === 'string' && shouldParseHTML(value)) {
+      obj[key] = {
+        type: 'nested-block',
+        children: parseHtml(value),
+      };
+      mutatedItems.push(parent);
     }
   });
+
+  mutateItemsAccordingToRules(data, mutatedItems);
 
   return data;
 }
