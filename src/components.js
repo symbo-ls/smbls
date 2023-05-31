7519ac7bfda0bdd0afacef50fcb80a4c3a628a70
nikoloza <nika.tomadze@gmail.com>
2023-05-31 07:11:34 +0400

    datepicker imports

--- src/components.js
+++ src/components.js
@@ -5,27 +5,26 @@ import {
   RadioWithLabel, ToggleSwithWithLabel
 } from "smbls"
 
-import { TimePicker } from "./components/TimePicker"
-import { DatePicker, DatePickerTwoColumns } from "./components/DatePicker"
-
 import designSystem from '@symbo.ls/default-config'
 console.log(designSystem)
+designSystem.globalTheme = 'dark'
 
 create({
   extend: Flex,
   props: { flow: 'column'},
 
-  checkBox: { extend: Checkbox },
-  radioButton: { extend: Radio },
-  toggleSwitch: { extend: ToggleSwitch },
-  checkBoxWithLabel: { extend: CheckBoxWithLabel },
-  radioWithLabel: { extend: RadioWithLabel },
-  toggleSwitchWithLabel: { extend: ToggleSwithWithLabel },
-  timePicker: { extend: TimePicker },
-  datePicker: { extend: DatePicker },
-  datePicker2: { extend: DatePickerTwoColumns }
+  // checkBox: { extend: Checkbox },
+  // radioButton: { extend: Radio },
+  // toggleSwitch: { extend: ToggleSwitch },
+  // checkBoxWithLabel: { extend: CheckBoxWithLabel },
+  // radioWithLabel: { extend: RadioWithLabel },
+  // toggleSwitchWithLabel: { extend: ToggleSwithWithLabel },
+  TimePicker: {},
+  DatePicker: {},
+  // datePicker2: { extend: DatePickerTwoColumns }
 }, {
-  designSystem: {
-    ...designSystem
-  },
+  designSystem,
+  state: {
+    globalTheme: 'dark',
+  }
 })
