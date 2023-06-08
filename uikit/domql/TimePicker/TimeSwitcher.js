445ef2a2acf985acbf4eb01d6bdfd2d192a5e073
nikoloza <nika.tomadze@gmail.com>
2023-06-08 08:30:47 +0400

    fixing timepicker

---
+++ uikit/domql/TimePicker/TimeSwitcher.js
@@ -0,0 +1,42 @@
+'use strict'
+
+import { Flex } from '@symbo.ls/atoms'
+import { Button } from '@symbo.ls/button'
+
+const props = {
+  boxSize: 'C B2',
+  flow: 'column',
+  overflow: 'hidden',
+  round: 'Z',
+  theme: 'secondary'
+}
+
+export const TimeSwitcher = {
+  props,
+  extend: Flex,
+
+  childExtend: {
+    extend: Button,
+    props: ({ state, key }) => ({
+      active: state.activeShift === key,
+      padding: '0',
+      flex: '1',
+      width: '100%',
+      fontSize: 'Y',
+      textTransform: 'uppercase',
+      round: '0',
+      background: 'transparent',
+      color: 'currentColor',
+      lineHeight: '1',
+      '.active': { theme: 'primary' }
+    }),
+    on: {
+      click: (event, element, state) => {
+        state.update({ activeShift: element.key })
+      }
+    }
+  },
+
+  am: { text: 'am' },
+  pm: { text: 'pm' }
+}
