bd96535f007db777a50289df85f9972eb29b5354
nikoloza <nika.tomadze@gmail.com>
2023-06-09 09:57:53 +0400

    added icontext readme, lint in markdown

--- uikit/plugins/Markdown/index.js
+++ uikit/plugins/Markdown/index.js
@@ -2,16 +2,16 @@
 
 import { markdownToDOMQL } from '@domql/converter'
 
-import { remark } from "remark";
-import remarkGfm from "remark-gfm";
+import { remark } from 'remark'
+import remarkGfm from 'remark-gfm'
 
 export const Markdown = {
   define: {
     $markdown: (param, el, state) => {
       if (!param) return
 
-      const markdownProcessor = remark().use(remarkGfm);
-      const mdast = markdownProcessor.parse(param);
+      const markdownProcessor = remark().use(remarkGfm)
+      const mdast = markdownProcessor.parse(param)
 
       const content = markdownToDOMQL(mdast)
       el.content = content
