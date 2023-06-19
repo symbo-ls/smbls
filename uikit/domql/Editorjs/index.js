4e8b95a8a1c3a8c85ccfbd865aad2b3206356825
nikoloza <nika.tomadze@gmail.com>
2023-06-20 01:18:49 +0400

    editorjs fallbacks

--- uikit/domql/Editorjs/index.js
+++ uikit/domql/Editorjs/index.js
@@ -1,17 +1,21 @@
 'use strict'
 
-import domqlConverterLib, { editorJsToDOMQL, initMutiny } from '@domql/converter'
+import { isArray, isObject } from '@domql/utils'
 
+import domqlConverterLib, { editorJsToDOMQL, initMutiny } from '@domql/converter'
 import { DomValueInterceptor } from './data/DomValueInterceptor'
 
 export const Editorjs = {
   define: {
     $editorjs: (param, el, state) => {
       if (!param) return
-      initMutiny()
+
+      if (isObject(param) && param.blocks) param = param.blocks
+      if (!isArray(param)) return
 
       const system = domqlConverterLib.getSystem()
       if (!system) {
+        initMutiny()
         const { interceptorApi } = system
         interceptorApi.addInterceptor(DomValueInterceptor, 'before')
       }
