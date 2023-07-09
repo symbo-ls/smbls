06ef956fcb036b33abe55af31b9d3eef7507eb92
nikoloza <nika.tomadze@gmail.com>
2023-07-09 10:21:19 +0400

    included utils in smbls

--- packages/smbls/index.js
+++ packages/smbls/index.js
@@ -6,6 +6,7 @@ export * from '@symbo.ls/emotion'
 export * from '@symbo.ls/init'
 export * from '@symbo.ls/create'
 export * from '@symbo.ls/uikit'
+export * from '@symbo.ls/utils'
 
 // export * from '@symbo.ls/socket/client' // TODO: Check with @nikoloza
 // export * from '@symbo.ls/socket-ui' // TODO: add it soon
