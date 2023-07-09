025a691087fc8281d97aebe30b893de7f0ea73bb
nikoloza <nika.tomadze@gmail.com>
2023-07-09 10:04:05 +0400

    cleanup dependencies

--- uikit/domql/Search/index.js
+++ uikit/domql/Search/index.js
@@ -1,6 +1,9 @@
 'use strict'
 
 import { Flex } from '@symbo.ls/atoms'
+import { Input } from '@symbo.ls/atoms'
+import { Icon } from '@symbo.ls/atoms'
+import { Button } from '@symbo.ls/atoms'
 
 export const Search = {
   extend: Flex,
@@ -24,34 +27,43 @@ export const Search = {
     }
   },
 
-  Icon: { icon: 'search' },
-
-  Input: {
-    placeholder: 'Type a command or search',
-    theme: null,
-    background: 'transparent',
-    color: 'currentColor',
-    flex: '1',
-    position: 'absolute',
-    boxSize: '100% 100%',
-    left: '0',
-    padding: '- - - C3',
-    ':hover': { background: 'transparent' },
-    ':focus': { background: 'transparent' },
-    ':focus + button': { opacity: '1' }
+  Icon: {
+    extend: Icon,
+    props: { icon: 'search' },
   },
 
-  Button_x: {
-    icon: 'x',
-    padding: 'Z2',
-    background: 'transparent',
-    color: 'white',
-    opacity: '0',
-    position: 'absolute',
-    right: 'A1',
-    Icon: {
-      width: 'X2',
-      height: 'X2'
+  Input: {
+    extend: Input,
+    props: {
+      placeholder: 'Type a command or search',
+      theme: null,
+      background: 'transparent',
+      color: 'currentColor',
+      flex: '1',
+      position: 'absolute',
+      boxSize: '100% 100%',
+      left: '0',
+      padding: '- - - C3',
+      ':hover': { background: 'transparent' },
+      ':focus': { background: 'transparent' },
+      ':focus + button': { opacity: '1' }
+    }
+  },
+
+  x: {
+    extend: Button,
+    props: {
+      icon: 'x',
+      padding: 'Z2',
+      background: 'transparent',
+      color: 'white',
+      opacity: '0',
+      position: 'absolute',
+      right: 'A1',
+      Icon: {
+        width: 'X2',
+        height: 'X2'
+      }
     }
   }
 }
