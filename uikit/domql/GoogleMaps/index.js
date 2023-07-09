491e80d61fb9270a09136eb7ad22febdc5107c1d
nikoloza <nika.tomadze@gmail.com>
2023-07-09 09:00:36 +0400

    textarea and maps update

--- uikit/domql/GoogleMaps/index.js
+++ uikit/domql/GoogleMaps/index.js
@@ -2,18 +2,19 @@
 
 import { Loader } from '@googlemaps/js-api-loader'
 
-let loader
 export const GoogleMaps = {
   props: {
     mapOptions: {
       disableDefaultUI: true
     }
   },
+  data: { loader: null },
   on: {
     init: (el, s, ctx) => {
-      const { version, apiKey } = el.props
-      if (loader) return
-      loader = new Loader({
+      const { data, props } = el
+      const { version, apiKey } = props
+      if (data.loader) return
+      data.loader = new Loader({
         apiKey: apiKey || process.env.GOOGLE_MAPS_API_KEY,
         version: version || 'weekly'
       })
@@ -21,7 +22,7 @@ export const GoogleMaps = {
     render: (el, s, ctx) => {
       const { props } = el
       const { mapOptions } = props
-      loader.load().then(async (google) => {
+      data.loader.load().then(async (google) => {
         const { Map } = await google.maps.importLibrary('maps')
         const map = new Map(el.node, mapOptions)
 
