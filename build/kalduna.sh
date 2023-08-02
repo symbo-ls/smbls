d6f0bea996c4a64f946cbf3b0afea7889cbc54b6
Nika Otiashvili <nikaoto@gmail.com>
2023-08-02 22:53:35 +0400

    cleanup old build files; cleanup after deprecating smbls convert

--- build/kalduna.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-# This script MUST be run from the root of the smbls repository
-
-GEN_DIR="${1:-uikit/react-generated}"
-SRC_DIR="uikit/domql"
-
-rimraf -I "$GEN_DIR" && \
-    smbls convert "$SRC_DIR" "$GEN_DIR" --react --internal-uikit "${@:2}"
