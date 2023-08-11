7b95682dba5c9c9f97fdedb1da6ff96aa6a39a84
svinchy <svinchy@gmail.com>
2023-08-11 23:14:36 +0400

    table name fix

--- uikit/domql/Table/index.js
+++ uikit/domql/Table/index.js
@@ -1,22 +1,3 @@
 'use strict'
 
-export const Video = {
-  tag: 'video',
-
-  childExtend: {
-    tag: 'source',
-    attr: {
-      src: ({ props }) => props.src,
-      controls: ({ props }) => props.controls
-    }
-  },
-
-  props: {
-    controls: true
-  },
-
-  attr: {
-    autoplay: ({ props }) => props.autoplay,
-    controls: ({ props }) => props.controls
-  }
-}
+export const Table = {}
