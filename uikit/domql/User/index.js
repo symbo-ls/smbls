057b8d010de25724e427930af9ccfe78d47da9e9
svinchy <svinchy@gmail.com>
2023-08-15 20:50:56 +0400

    add user component

--- uikit/domql/User/index.js
+++ uikit/domql/User/index.js
@@ -1,3 +1,37 @@
 'use strict'
+import { Flex } from '@symbo.ls/atoms'
+import { Avatar } from '../Avatar'
 
-export const User = {}
+export const User = {
+  extend: Flex,
+  image: { extend: Avatar },
+  content: {
+    extend: Flex,
+    heading: { name: { text: 'erin schleifer' } },
+    paragraph: { mail: { props: { text: 'email@symbols.com' } } }
+  },
+
+  props: {
+    align: 'center flex-start',
+    width: 'fit-content',
+    gap: 'A',
+
+    image: { boxSize: 'B1 B1' },
+
+    content: {
+      flow: 'column',
+      heading: {
+        name: {
+          fontSize: 'Z1',
+          textTransform: 'capitalize'
+        }
+      },
+      paragraph: {
+        mail: {
+          fontSize: 'Y1',
+          color: '#A3A3A8'
+        }
+      }
+    }
+  }
+}
