3dd08c7ddbd03367d311f4619a639ae39c0e7f49
svinchy <svinchy@gmail.com>
2023-08-25 15:31:05 +0400

    add code field component

--- uikit/domql/Field/index.js
+++ uikit/domql/Field/index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { Focusable } from '@symbo.ls/atoms'
+import { Focusable, Flex } from '@symbo.ls/atoms'
 import { IconText } from '@symbo.ls/icon'
 import { Input } from '@symbo.ls/input'
 
@@ -38,4 +38,66 @@ export const Field = {
   }),
 
   input: { extend: [Focusable, Input] }
+}
+
+export const CustomizedField = {
+  extend: Field,
+  props: {
+    background: 'transparent',
+    border: '1px solid #3F3F43',
+    padding: '0',
+    minHeight: 'C+W',
+    round: 'Z',
+    overflow: 'hidden',
+    input: {
+      background: 'transparent',
+      placeholder: 'placeholder',
+      color: 'white',
+      fontSize: 'Z',
+      round: '0',
+      '::placeholder': { textTransform: 'capitalize' }
+    }
+  }
+}
+
+export const CodeField = {
+  extend: Flex,
+  childExtend: {
+    extend: CustomizedField,
+    props: {
+      boxSize: 'D D',
+      input: {
+        type: 'number',
+        placeholder: '0',
+        textAlign: 'center',
+        fontSize: 'E'
+      },
+      style: { 'input[type=number]::-webkit-inner-spin-button': { '-webkit-appearance': ' none' } }
+    }
+  },
+  ...[{}, {}, {}, {}],
+
+  props: {
+    align: 'center flex-start',
+    gap: 'A'
+  }
+}
+
+export const FieldWithTitle = {
+  extend: Flex,
+  title: {
+    tag: 'h6',
+    props: { text: 'Old password' }
+  },
+  field: { extend: CustomizedField },
+
+  props: {
+    flow: 'column',
+    gap: 'Y',
+    title: {
+      fontSize: 'Z',
+      color: '#CFCFD1',
+      padding: '- - - Y+W'
+    }
+  }
 }
---
+++ uikit/domql/Modal/index.js
@@ -0,0 +1,7 @@
+'use strict'
+
+import { Field } from '@symbo.ls/field'
+
+// export const ChangePassword = {
+//   extend: Field
+// }
