f50a42a977562524d24578afbc698c0858c33394
svinchy <svinchy@gmail.com>
2023-09-11 12:46:05 +0400

    add circle progress bar

--- uikit/domql/Progress/index.js
+++ uikit/domql/Progress/index.js
@@ -23,3 +23,54 @@ export const ProgressLine = {
     }
   }
 }
+
+export const ProgressCircle = {
+  progress: {
+    attr: {
+      max: ({ props }) => props.max,
+      progress: ({ props }) => props.progress,
+      value: ({ props }) => props.value
+    }
+  },
+
+  progressValue: { props: { text: '70%' } },
+
+  props: {
+    boxSize: 'D D',
+    position: 'relative',
+    ':after': {
+      content: '""',
+      position: 'absolute',
+      width: 'calc(100% - 8px)',
+      height: 'calc(100% - 8px)',
+      background: 'black',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      round: '100%'
+    },
+
+    progress: {
+      boxSize: '100% 100%',
+      value: '.7',
+      round: '100%',
+      overflow: 'hidden',
+      style: {
+        '&::-webkit-progress-bar': { background: '#0474F2' },
+        '&::-webkit-progress-value': {
+          background: '#252527',
+          borderRadius: '2px'
+        }
+      }
+    },
+
+    progressValue: {
+      position: 'absolute',
+      top: '50%',
+      left: '50%',
+      transform: 'translate(-50%, -50%)',
+      zIndex: '3',
+      fontSize: 'Z'
+    }
+  }
+}
