1d5209b007ab74a31165f8054976b1a833872c87
nikoloza <nika.tomadze@gmail.com>
2023-09-25 02:23:22 +0400

    added default icons

--- uikit/icons/default-icons/index.js
+++ uikit/icons/default-icons/index.js
@@ -1,5 +1,34 @@
 'use strict'
 
 export default {
-  symbols: require('./src/logo.svg')
+  symbols: require('./src/logo.svg'),
+  arrowDownCircle: require('./src/arrow-down-circle.svg'),
+  arrowDownLeft: require('./src/arrow-down-left.svg'),
+  arrowDownRight: require('./src/arrow-down-right.svg'),
+  arrowDown: require('./src/arrow-down.svg'),
+  arrowLeftCircle: require('./src/arrow-left-circle.svg'),
+  arrowLeft: require('./src/arrow-left.svg'),
+  arrowRight: require('./src/arrow-right.svg'),
+  arrowRightCircle: require('./src/arrow-right-circle.svg'),
+  arrowUpCircle: require('./src/arrow-up-circle.svg'),
+  arrowUpLeft: require('./src/arrow-up-left.svg'),
+  arrowUpRight: require('./src/arrow-up-right.svg'),
+  arrowUp: require('./src/arrow-up.svg'),
+  checkCircle: require('./src/check-circle.svg'),
+  check: require('./src/check.svg'),
+  chevronDown: require('./src/chevron-down.svg'),
+  chevronLeft: require('./src/chevron-left.svg'),
+  chevronRight: require('./src/chevron-right.svg'),
+  chevronUp: require('./src/chevron-up.svg'),
+  dribbble: require('./src/dribbble.svg'),
+  eyeOff: require('./src/eye-off.svg'),
+  eye: require('./src/eye.svg'),
+  file: require('./src/file.svg'),
+  info: require('./src/info.svg'),
+  logo: require('./src/logo.svg'),
+  minus: require('./src/minus.svg'),
+  moon: require('./src/moon.svg'),
+  moreHorizontal: require('./src/more-horizontal.svg'),
+  moreVertical: require('./src/more-vertical.svg'),
+  plus: require('./src/plus.svg')
 }
