75a01f350c7fbe87be94940dde577155f4e22018
nikoloza <nika.tomadze@gmail.com>
2023-09-26 17:23:36 +0400

    naming fix

---
+++ uikit/domql/Input/Textarea_.js
@@ -0,0 +1,62 @@
+'use strict'
+
+import { Flex } from '@symbo.ls/atoms'
+import { Input } from './Input'
+
+export const Textarea = {
+  tag: 'textarea',
+  extend: [Input, Flex],
+  props: {
+    border: '1px solid #3F3F43',
+    background: 'transparent',
+    round: 'Y+W',
+    width: 'G1',
+    height: 'D2+W',
+    lineHeight: 1.4,
+    placeholder: 'Leave us a message...',
+    fontSize: 'Y2',
+    outline: 'none !important',
+    color: 'white',
+    style: { resize: 'none' }
+  }
+}
+
+export const TextareaWithTitle = {
+  extend: Flex,
+  props: {
+    flow: 'column',
+    boxSize: 'fit-content fit-content',
+    gap: 'Y+W'
+  },
+  Title: {
+    props: {
+      text: 'Label',
+      fontSize: 'Z',
+      lineHeight: '1em',
+      padding: '- - - Z',
+      color: 'gray4'
+    }
+  },
+  Textarea: {}
+}
+
+export const TextareaWithButton = {
+  extend: Flex,
+  props: { gap: 'Y2' },
+  Textarea: {
+    height: '52px',
+    padding: 'A',
+    fontSize: 'Z1',
+    color: 'white',
+    placeholder: 'Message',
+    round: 'Z2'
+    // background: 'gray'
+
+  },
+  IconCommonButton: {
+    props: {
+      background: 'blue',
+      icon: { name: 'send' }
+    }
+  }
+}
