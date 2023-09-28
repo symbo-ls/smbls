7e1a358c592d7a0228977d52d281651785b7da7f
nikoloza <nika.tomadze@gmail.com>
2023-09-29 03:03:07 +0400

    linkable packages generation script added

--- packages/cli/bin/link-packages.js
+++ packages/cli/bin/link-packages.js
@@ -1,84 +1,46 @@
 #!/usr/bin/env node
 
-import { execSync } from 'child_process'
+import fs from 'fs'
+import { exec, execSync } from 'child_process'
 import { program } from './program.js'
 
-const packagesToLink = [
-  '@symbo.ls/create',
-  '@symbo.ls/convert',
-  '@symbo.ls/cli',
-  'attrs-in-props',
-  'css-in-props',
-  '@symbo.ls/default-config',
-  '@symbo.ls/init',
-  '@symbo.ls/emotion',
-  '@symbo.ls/fetch',
-  'smbls',
-  '@symbo.ls/scratch',
-  '@symbo.ls/socket-ui',
-  '@symbo.ls/socket',
-  '@symbo.ls/utils',
-  '@symbo.ls/react',
-  '@symbo.ls/uikit',
-  '@symbo.ls/avatar',
-  '@symbo.ls/atoms',
-  '@symbo.ls/board',
-  '@symbo.ls/box',
-  '@symbo.ls/button',
-  '@symbo.ls/card',
-  '@symbo.ls/datepicker',
-  '@symbo.ls/dialog',
-  '@symbo.ls/doublehr',
-  '@symbo.ls/dropdown',
-  '@symbo.ls/editorjs',
-  '@symbo.ls/field',
-  '@symbo.ls/form',
-  '@symbo.ls/google-maps',
-  '@symbo.ls/helmet',
-  '@symbo.ls/icon',
-  '@symbo.ls/indicator',
-  '@symbo.ls/input',
-  '@symbo.ls/label',
-  '@symbo.ls/link',
-  '@symbo.ls/list',
-  '@symbo.ls/markdown',
-  '@symbo.ls/modal',
-  '@symbo.ls/notification',
-  '@symbo.ls/paragraphbutton',
-  '@symbo.ls/pills',
-  '@symbo.ls/progress',
-  '@symbo.ls/range',
-  '@symbo.ls/select',
-  '@symbo.ls/sidebar',
-  '@symbo.ls/slider',
-  '@symbo.ls/sociallink',
-  '@symbo.ls/steps',
-  '@symbo.ls/tab',
-  '@symbo.ls/threejs',
-  '@symbo.ls/table',
-  '@symbo.ls/timepicker',
-  '@symbo.ls/titleparagraph',
-  '@symbo.ls/unitvalue',
-  '@symbo.ls/tooltip',
-  '@symbo.ls/upload',
-  '@symbo.ls/user',
-  '@symbo.ls/video',
-  '@symbo.ls/default-icons',
-  '@symbo.ls/feather-icons',
-  '@symbo.ls/fluent-icons',
-  '@symbo.ls/material-icons',
-  '@symbo.ls/react-atoms',
-  '@symbo.ls/react-box',
-  '@symbo.ls/react-icon',
-  '@symbo.ls/react-provider'
-]
+import packages from './linking/packages.js'
+
+const COMMAND = 'npx lerna exec -- cat package.json | jq \'.name\''
+const capture = (opts) => {
+  exec(COMMAND, (error, stdout, stderr) => {
+    if (error) {
+      console.error(`Error executing command: ${error}`)
+      return
+    }
+
+    const packageNames = stdout
+      .split('\n')
+      .map((line) => line.trim().replaceAll('"', '\''))
+      .filter(Boolean)
+
+    const output = `export default [
+  ${packageNames.map((name) => `${name}`).join(',\n  ')}
+]\n`
+
+    try {
+      fs.writeFileSync(process.cwd() + '/packages/cli/bin/linking/packages.js', output)
+      console.log(`Packages list with ${packageNames.length} items successfully created\n`)
+    } catch (e) {
+      console.error('Error writing to file:')
+      console.error(e)
+    }
+  })
+}
 
 program
   .command('link-packages')
   .description('Run "yarn link" on specified packages')
-  .action(() => {
+  .option('-c, --capture', 'Capture and write all package names.')
+  .action((opts) => {
+    if (opts.capture) return capture(opts)
     try {
-      for (const packageName of packagesToLink) {
+      for (const packageName of packages) {
         console.log(`Linking ${packageName}...`)
         execSync(`yarn link ${packageName} --force`, { stdio: 'inherit' })
       }
