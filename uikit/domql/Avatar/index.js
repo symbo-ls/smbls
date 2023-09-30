52393d4e56ce55084b8b1eb7af2624bc6ca285b8
nikoloza <nika.tomadze@gmail.com>
2023-09-30 05:20:48 +0400

    avatar boxSize overrite, adding few attrs to iframe

--- uikit/domql/Avatar/index.js
+++ uikit/domql/Avatar/index.js
@@ -1,64 +1,6 @@
 'use strict'
 
-import { Button } from '@symbo.ls/button'
-
 export * from './Avatar'
 export * from './AvatarIndicator'
 export * from './AvatarBundle'
-
-export const AvatarChooser = {
-  extend: Button,
-  tag: 'label',
-
-  props: {
-    round: 'C',
-    gap: 'Y',
-    padding: 'W2 A W2 W2',
-    theme: 'tertiary',
-    position: 'relative',
-    cursor: 'pointer'
-  },
-
-  Avatar: {
-    props: ({ state }) => ({
-      key: state.key,
-      boxSize: 'B1',
-      pointerEvents: 'none'
-    })
-  },
-
-  Select: {
-    props: {
-      outline: 'none',
-      pointerEvents: 'all',
-      appearance: 'none',
-      border: 'none',
-      width: '100%',
-      height: '100%',
-      background: 'none',
-      color: 'currentColor',
-      fontSize: 'A',
-      lineHeight: 1,
-      margin: '0 0 0 -B1+X',
-      padding: '0 A 0 B1+X',
-      ':focus-visible': {
-        outline: 'none'
-      }
-    },
-
-    childExtend: { tag: 'option' },
-    $setPropsCollection: ({ parent, state }) => {
-      return parent.props.options.map(v => {
-        if (v.text === state.key) return { ...v, selected: true }
-        return v
-      })
-    },
-
-    on: {
-      change: (ev, { state }) => {
-        state.update({ key: ev.target.value })
-        console.log(state.key)
-      }
-    }
-  }
-}
+export * from './AvatarChooser'
