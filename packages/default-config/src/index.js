3e2d485e2caa6b0098f525670c496075429454f2
nikoloza <nika.tomadze@gmail.com>
2023-11-20 04:37:35 +0400

    def config responsive

--- packages/default-config/src/index.js
+++ packages/default-config/src/index.js
@@ -6,6 +6,7 @@ import { THEME } from './theme'
 import { TYPOGRAPHY } from './typography'
 import { SPACING } from './spacing'
 import { FONT, FONT_FAMILY } from './font'
+import { MEDIA } from './media'
 
 export const DEFAULT_CONFIG = {
   version: '0.0.1',
@@ -18,25 +19,7 @@ export const DEFAULT_CONFIG = {
   FONT_FAMILY,
   TIMING: {},
   ICONS,
-  MEDIA: {
-    tv: '(min-width: 2780px)',
-
-    screenL: '(max-width: 1920px)',
-    screenM: '(max-width: 1680px)',
-    screenS: '(max-width: 1440px)',
-    tabletL: '(max-width: 1366px)',
-    tabletM: '(max-width: 1280px)',
-    tabletS: '(max-width: 1024px)',
-    mobileL: '(max-width: 768px)',
-    mobileM: '(max-width: 560px)',
-    mobileS: '(max-width: 480px)',
-    mobileXS: '(max-width: 375px)',
-
-    light: '(prefers-color-scheme: light)',
-    dark: '(prefers-color-scheme: dark)',
-
-    print: 'print'
-  },
+  MEDIA,
   DEVICES: {},
   CASES: {},
   SVG: {} // TODO: Check with @nikoloza on this
