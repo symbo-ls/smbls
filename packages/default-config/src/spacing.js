f5064a30f19ea2d5a3068696e5c578bce6649b92
nikoloza <nika.tomadze@gmail.com>
2023-11-21 05:33:18 +0400

    added spacing ratios

--- packages/default-config/src/spacing.js
+++ packages/default-config/src/spacing.js
@@ -1,14 +1,12 @@
 'use strict'
 
 export const SPACING = {
-  base: 16,
   ratio: 1.618,
-  subSequence: true
-  // '@tabletS': {
-  //   base: 30,
-  //   ratio: 1.618
-  // }
-  // '@mobileM': {
-  //   ratio: 1.2
-  // }
+  subSequence: true,
+  '@tabletS': {
+    ratio: 1.618
+  },
+  '@mobileM': {
+    ratio: 1.2
+  }
 }
