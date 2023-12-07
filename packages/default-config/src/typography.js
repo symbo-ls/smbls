4741ee6c0f4c4cf8af20d1bc6dea5f0023247734
nikoloza <nika.tomadze@gmail.com>
2023-12-07 16:09:57 +0400

    rm type

--- packages/default-config/src/typography.js
+++ packages/default-config/src/typography.js
@@ -4,12 +4,5 @@ export const TYPOGRAPHY = {
   base: 16,
   ratio: 1.25,
   subSequence: true,
-  templates: {},
-  '@tabletS': {
-    base: 15,
-    ratio: 1.23
-  }
-  // '@mobileM': {
-  //   ratio: 1.125
-  // }
+  templates: {}
 }
