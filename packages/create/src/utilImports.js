330c3248a282106abbf16631fa85f79497f6d752
nikoloza <nika.tomadze@gmail.com>
2023-12-26 23:24:18 +0400

    added applycss

--- packages/create/src/utilImports.js
+++ packages/create/src/utilImports.js
@@ -5,6 +5,6 @@ import { scratchUtils, scratchSystem, set } from '@symbo.ls/scratch'
 export { scratchUtils, scratchSystem, set }
 export * from '@domql/utils'
 export * from '@symbo.ls/utils'
-export { init, reInit } from '@symbo.ls/init'
+export { init, reInit, applyCSS } from '@symbo.ls/init'
 export * from '@domql/report'
 export * from '@domql/router'
