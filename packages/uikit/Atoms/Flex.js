f060232164d9e87ca6e53791e0ef9fca6d413f01
nikoloza <nika.tomadze@gmail.com>
2024-02-19 01:35:55 +0400

    added flex xy flow

--- packages/uikit/Atoms/Flex.js
+++ packages/uikit/Atoms/Flex.js
@@ -1,12 +1,24 @@
 'use strict'
 
+import { isUndefined } from '@domql/utils'
+
 export const Flex = {
   props: {
     display: 'flex'
   },
 
   class: {
-    flow: ({ props }) => props.flow && ({ flexFlow: props.flow + (props.reverse ? '-reverse' : '') }),
+    flow: ({ props }) => {
+      const { flow } = props
+      if (isUndefined(flow)) return
+      let [direction, wrap] = flow.split(' ')
+      if (flow.startsWith('x') || flow.startsWith('row')) direction = 'row'
+      if (flow.startsWith('y') || flow.startsWith('column')) direction = 'column'
+      return {
+        flexFlow: (direction || '') + (props.reverse ? '-reverse' : '') + ' ' + (wrap || '')
+      }
+    },
+
     wrap: ({ props }) => props.wrap && ({ flexWrap: props.wrap }),
     align: ({ props }) => {
       if (typeof props.align !== 'string') return
