bf6ea8cdcc4652f7008f1142411a06fdea6ef486
nikoloza <nika.tomadze@gmail.com>
2024-02-21 23:33:47 +0400

    fixed flow

--- packages/uikit/Atoms/Flex.js
+++ packages/uikit/Atoms/Flex.js
@@ -1,6 +1,6 @@
 'use strict'
 
-import { isUndefined } from '@domql/utils'
+import { isString } from '@domql/utils'
 
 export const Flex = {
   props: {
@@ -10,18 +10,18 @@ export const Flex = {
   class: {
     flow: ({ props }) => {
       const { flow, reverse } = props
-      if (isUndefined(flow) && isUndefined(reverse)) return
+      if (!isString(flow)) return
       let [direction, wrap] = (flow || 'row').split(' ')
       if (flow.startsWith('x') || flow.startsWith('row')) direction = 'row'
       if (flow.startsWith('y') || flow.startsWith('column')) direction = 'column'
       return {
-        flexFlow: (direction || '') + (props.reverse ? '-reverse' : '') + ' ' + (wrap || '')
+        flexFlow: (direction || '') + (reverse ? '-reverse' : '') + ' ' + (wrap || '')
       }
     },
 
     wrap: ({ props }) => props.wrap && ({ flexWrap: props.wrap }),
     align: ({ props }) => {
-      if (typeof props.align !== 'string') return
+      if (!isString(props.align)) return
       const [alignItems, justifyContent] = props.align.split(' ')
       return { alignItems, justifyContent }
     }
