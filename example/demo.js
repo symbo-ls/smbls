456818950ceb5f850f2a4a59953d4bf484456fbb
nikoloza <nika.tomadze@gmail.com>
2024-02-29 06:25:37 +0400

    added routes on demo

--- example/demo.js
+++ example/demo.js
@@ -3,14 +3,13 @@
 import { create } from '@symbo.ls/create'
 import { StyleguidePage, ComponentsPage } from '@symbo.ls/preview' // eslint-disable-line
 
-// import designSystem from '@symbo.ls/config' // eslint-disable-line
-
 create({
-  extend: StyleguidePage,
-
   state: {
     globalTheme: 'dark'
   }
 }, {
-  // designSystem
+  pages: {
+    '/': StyleguidePage,
+    '/components': ComponentsPage
+  }
 })
