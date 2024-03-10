2a133bf6429a0fc8be39cf00b445f706051f6bb4
nikoloza <nika.tomadze@gmail.com>
2024-03-10 19:13:07 +0400

    fix example

--- example/demo.js
+++ example/demo.js
@@ -2,10 +2,14 @@
 
 import { create } from '@symbo.ls/create'
 import { StyleguidePage, ComponentsPage } from '@symbo.ls/preview' // eslint-disable-line
-import * as data from '../toko'
 
 create({
   state: {
     globalTheme: 'dark'
   }
-}, data)
+}, {
+  pages: {
+    '/': StyleguidePage,
+    '/components': ComponentsPage
+  }
+})
