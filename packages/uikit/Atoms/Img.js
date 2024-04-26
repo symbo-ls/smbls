fa46be65d646a1eca9c5ce3e86b77ce549891c83
nikoloza <nika.tomadze@gmail.com>
2024-04-26 08:15:21 +0400

    new url guard on image

--- packages/uikit/Atoms/Img.js
+++ packages/uikit/Atoms/Img.js
@@ -6,11 +6,13 @@ export const Img = {
   attr: {
     src: ({ props, context }) => {
       const src = props.src
-      const file = context.files[src]
+      let isUrl
+      try { isUrl = new URL(src) } catch (e) {}
+      if (isUrl) return src
+      const file = context.files && context.files[src]
       if (src.startsWith('/') && file) {
         return file.content.src
       }
-      return src
     },
     alt: ({ props }) => props.alt,
     title: ({ props }) => props.title || props.alt
