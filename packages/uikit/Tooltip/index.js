18381fe9330c52e2d98702a0fbe394db2125bd16
nikoloza <nika.tomadze@gmail.com>
2024-05-11 11:09:33 +0400

    fix tooltip parent behaviour

--- packages/uikit/Tooltip/index.js
+++ packages/uikit/Tooltip/index.js
@@ -45,7 +45,7 @@ export const Tooltip = {
 }
 
 export const TooltipHidden = {
-  extend: Tooltip,
+  extend: 'Tooltip',
 
   props: ({ props }) => ({
     position: 'absolute',
@@ -108,19 +108,17 @@ export const TooltipParent = {
   props: (el) => {
     const { Tooltip, TooltipHidden } = el
     const TooltipElem = (Tooltip || TooltipHidden)
-    if (!TooltipElem || TooltipElem) return
+    if (!TooltipElem) return
     const TooltipActive = TooltipElem && TooltipElem.props && TooltipElem.props['.active']
     return {
       position: 'relative',
       zIndex: 999,
-      style: {
-        '&:hover, &:focus-visible': {
-          zIndex: 1000,
-          '& [tooltip]': TooltipActive || {
-            transform: 'translate3d(-50%,0,0)',
-            opacity: 1,
-            visibility: 'visible'
-          }
+      ':hover, &:focus-visible': {
+        zIndex: 1000,
+        '& [tooltip]': TooltipActive || {
+          transform: 'translate3d(-50%,0,0)',
+          opacity: 1,
+          visibility: 'visible'
         }
       }
     }
