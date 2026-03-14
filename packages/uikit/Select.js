9372fbc441a966d7545bb919602b120c49713a19
nikoloza <nika.tomadze@gmail.com>
2026-03-14 11:04:59 +0400

    cleanup uikit packages, fixed brender error

---
+++ packages/uikit/Select.js
@@ -0,0 +1,33 @@
+'use strict'
+
+export const Select = {
+  extends: 'Focusable',
+  tag: 'select',
+
+  fontSize: 'A',
+  border: 'none',
+  boxSizing: 'border-box',
+  theme: 'field',
+  cursor: 'pointer',
+  childProps: {
+    tag: 'option',
+    attr: {
+      value: ({ props }) => props.value,
+      selected: ({ props }) => props.selected,
+      disabled: ({ props }) => props.disabled
+    }
+  },
+
+  attr: {
+    name: ({ props }) => props.name,
+    disabled: ({ props }) => props.disabled,
+    value: el => {
+      if (!el.props || !el.props.value) return
+      const val = el.call('exec', el.props.value, el)
+      if (el.call('isString', val) && val.includes('{{')) {
+        return el.call('replaceLiteralsWithObjectFields', val)
+      }
+      return val
+    }
+  }
+}
